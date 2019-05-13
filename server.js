7cdaead637e4a8446cabeb46b1ce3801e1fc8e2f
bcoggins78 <bcoggins78@gmail.com>
2019-05-13 15:50:38 -0400

    Updated routes and configured for Sequelize

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+var db = require("./models");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,10 +18,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+require("./controllers/burgersController.js")(app);
 
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
